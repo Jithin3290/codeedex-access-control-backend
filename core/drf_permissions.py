253db1f8958bb8f70411d2cc260470c6ec84a867
Jithin3290 <jithin3290@gmail.com>
2026-01-08 00:23:42 +0530

    done

--- core/drf_permissions.py
+++ core/drf_permissions.py
@@ -2,11 +2,18 @@ from rest_framework.permissions import BasePermission
 from core.permissions import get_user_permissions
 
 class HasPermission(BasePermission):
-    required_permission = None
-
     def has_permission(self, request, view):
-        if not self.required_permission:
+        required = getattr(view, "required_permission", None)
+        if not required:
             return True
 
-        perms = get_user_permissions(request.user)
-        return any(p[0] == self.required_permission for p in perms)
+        user = request.user
+
+        # collect permissions from roles
+        role_perms = Permission.objects.filter(
+            roles__users=user,
+            code=required
+        ).exists()
+
+        return role_perms
+
