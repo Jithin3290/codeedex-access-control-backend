e2f66cdf4b1dcebedaf5ab99c4e816b27fc64305
Jithin3290 <jithin3290@gmail.com>
2026-01-08 03:02:04 +0530

    final commit

--- core/scope.py
+++ core/scope.py
@@ -1,9 +1,13 @@
 from teams.models import TeamMembership
 from core.permissions import get_user_permissions
 
-def filter_queryset_by_scope(user, queryset, permission_code):
-    perms = get_user_permissions(user)
 
+def filter_queryset_by_scope(user, queryset, permission_code):
+    # Superuser sees everything
+    if user.is_superuser:
+        return queryset
+
+    perms = get_user_permissions(user)
     scopes = {scope for code, scope in perms if code == permission_code}
 
     if not scopes:
@@ -13,11 +17,12 @@ def filter_queryset_by_scope(user, queryset, permission_code):
         return queryset
 
     if "team" in scopes:
-        try:
-            team = user.teammembership.team
-            return queryset.filter(teammembership__team=team)
-        except TeamMembership.DoesNotExist:
+        memberships = TeamMembership.objects.filter(user=user)
+        if not memberships.exists():
             return queryset.none()
+
+        teams = memberships.values_list("team", flat=True)
+        return queryset.filter(teammembership__team__in=teams)
 
     if "self" in scopes:
         return queryset.filter(id=user.id)
